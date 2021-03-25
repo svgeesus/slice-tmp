bdeb4d9a87661a5acebc3f701cccd9f205435989
Chris Lilley <chris@w3.org>
2021-03-24 21:07:58 -0400

    oh fuck it is all broken

--- slices-ok.js
+++ slices-ok.js
@@ -1,24 +1,27 @@
 import Color from "https://colorjs.io/color.js";
 
 let rgbspace = "sRGB"; // get from form
-let hue = 301.37; // get from form
+let hue = 264.0585; // blue
+// let hue = 109.7874 // yellow
+// let hue = 29.2271; // red
 
 let slice = [];
 let boundary = [];
 let edge;
 let max_chroma = 0;
 
-
+// scale factors - lightness 100, chroma 400
 let lightness, chroma;
 let fill = "red";
 console.time();
-let step = .5;
+let lstep = .005;
+let cstep = .001;
 
-for (lightness = 0; lightness <= 100; lightness += step) {
+for (lightness = 0; lightness <= 1; lightness += lstep) {
     edge = undefined;
 
-    for (chroma = 0; chroma <= 140; chroma += step) {
-        let swatch = new Color("lch", [lightness, chroma, hue]);
+    for (chroma = 0.27; chroma <= 0.35; chroma += cstep) {
+        let swatch = new Color("oklch", [lightness, chroma, hue]);
         let rgb = swatch.to("sRGB");
         // console.log({swatch, rgb});
         if (rgb.inGamut("srgb")) {
@@ -34,22 +37,22 @@ for (lightness = 0; lightness <= 100; lightness += step) {
                 }
             }
         };
-        slice.push(`<rect x='${chroma - 1.1 * step / 2}' y='${-lightness - 1.1 * step / 2}' width='${1.1 * step}' height='${1.1 * step}' fill='${fill}' />`);
+        slice.push(`<rect x='${400 * chroma  - 1.1 * cstep / 2}' y='${-lightness * 100 - 1.1 * lstep / 2}' width='${1.1 * cstep * 400}' height='${1.1 * lstep * 100}' fill='${fill}' />`);
     };
-    boundary.push(`${edge}, ${-lightness} `);
+    boundary.push(`${edge * 400}, ${-lightness * 100} `);
 };
 console.timeEnd();
 
 let l_axis = [];
 
-for (lightness = 0; lightness <= 100; lightness+=25) {
-    l_axis.push(`<text x='-2' y='${-lightness + 1}'>${lightness}</text>`);
+for (lightness = 0; lightness <= 1; lightness+=0.25) {
+    l_axis.push(`<text x='-2' y='${-lightness * 100 + 1}'>${lightness.toFixed(2)}</text>`);
 }
 
 let c_axis = [];
 
-for (chroma = 0; chroma <= 140; chroma+=25) {
-    c_axis.push(`<text x='${chroma}' y='5'>${chroma}</text>`);
+for (chroma = 0; chroma <= 0.35; chroma+=.05) {
+    c_axis.push(`<text x='${chroma * 400}' y='5'>${chroma.toFixed(2)}</text>`);
 }
 
 let markup = `<svg xmlns='http://www.w3.org/2000/svg' viewBox='-14 -108 158 115'>
@@ -58,8 +61,8 @@ stroke-width='2'/>
 <g stroke='none'>
 ${slice.join('\n')}
 </g>
-<polyline fill='none' stroke='#AAA' stroke-width='0.3' points='${max_chroma}, 0.5, ${max_chroma}, -100.5' />
-<polyline fill='none' stroke='white' stroke-width='0.7' stroke-linecap='round' points='-0.5,0 ${boundary.join('\n')} -0.5,-100' />
+<polyline fill='none' stroke='#AAA' stroke-width='0.3' points='${max_chroma * 400}, 0.5, ${max_chroma * 400}, -100.5' />
+<polyline fill='none' stroke='white' stroke-width='0.7' stroke-linecap='round' xpoints='-0.5,0 ${boundary.join('\n')} -0.5,-100' />
 <g font-family='Helvetica Neue, Helvetica, sans-serif' fill='white'>
 <g font-size='3' text-anchor='end'>
 ${l_axis.join('\n')}
@@ -69,10 +72,10 @@ ${c_axis.join('\n')}
 </g>
 <g font-size='6' text-anchor='end'>
  <text x='134' y='-90'>Hue: ${hue}°</text>
- <text x='134' y='-14'>CIE LCH</text>
+ <text x='134' y='-14'>OK LCH</text>
  <text x='134' y='-5'>Gamut: ${rgbspace}</text>
 </g>
-<text x='${max_chroma}' y='-102' text-anchor='middle' font-size='4'>${max_chroma}</text>
+<text x='${max_chroma * 400}' y='-102' text-anchor='middle' font-size='4'>${max_chroma.toFixed(3)}</text>
 </g>
 </svg>`;
 
